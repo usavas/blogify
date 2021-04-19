35eb4a9e69a221c35c1f0599200e9d881c9a3b58
fsavas <savasusame@gmail.com>
2021-04-19 12:21:59 +0300

    make the home page dynamic

--- routes/home_router.js
+++ routes/home_router.js
@@ -1,9 +1,23 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+posts = [
+  {
+    title: "What a wonderful life",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos accusamus consequatur repudiandae exercitationem tempore magni nesciunt cupiditate sit voluptatum consequuntur alias voluptatibus, nobis, vitae, repellat nihil rem sed fugiat iusto!",
+  },
+  {
+    title: "Minimalism",
+    content:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos accusamus consequatur repudiandae exercitationem tempore magni nesciunt cupiditate sit voluptatum consequuntur alias voluptatibus, nobis, vitae, repellat nihil rem sed fugiat iusto!",
+  },
+];
+
+categories = ["Lifestyle", "Home Design", "Technology", "Food for Thought"];
+
 router.get("/", function (req, res, next) {
-  res.render("home", { title: "Home" });
+  res.render("home", { posts: posts, categories: categories });
 });
 
 module.exports = router;
