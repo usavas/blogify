5892f8f4338416a390d1378c3621c6791bf5daca
fsavas <savasusame@gmail.com>
2021-04-19 13:29:42 +0300

    create post page and route

--- routes/home_router.js
+++ routes/home_router.js
@@ -3,14 +3,18 @@ var router = express.Router();
 
 posts = [
   {
+    id: 1,
     title: "What a wonderful life",
     content:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos accusamus consequatur repudiandae exercitationem tempore magni nesciunt cupiditate sit voluptatum consequuntur alias voluptatibus, nobis, vitae, repellat nihil rem sed fugiat iusto!",
+    postRoute: "/post/1",
   },
   {
+    id: 2,
     title: "Minimalism",
     content:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos accusamus consequatur repudiandae exercitationem tempore magni nesciunt cupiditate sit voluptatum consequuntur alias voluptatibus, nobis, vitae, repellat nihil rem sed fugiat iusto!",
+    postRoute: "/post/2",
   },
 ];
 
@@ -18,6 +22,19 @@ categories = ["Lifestyle", "Home Design", "Technology", "Food for Thought"];
 
 router.get("/", function (req, res, next) {
   res.render("home", { posts: posts, categories: categories });
+});
+
+router.get("/post/:id", function (req, res, next) {
+  let post = posts.filter((p) => p.id === parseInt(req.params.id))[0];
+  res.render("post", { post: post });
+});
+
+router.get("/about", function (req, res, next) {
+  res.render("about");
+});
+
+router.get("/privacy", function (req, res, next) {
+  res.render("privacy");
 });
 
 module.exports = router;
